c8eaad61428814ba0a81764ef3d35402e09cd557
Maartje Eyskens <meyskens@me.com>
2016-02-14 16:48:22 +0100

    Refactor server.js

--- intern/HTTP/server.js
+++ intern/HTTP/server.js
@@ -1,39 +1,39 @@
-var app = require("express")(),
-	fs = require("fs")
-if (global.config.httpsPort !== 0) {
-	var https = require('http2').createServer({
-		key: fs.readFileSync(global.config.httpsKey),
-		cert: fs.readFileSync(global.config.httpsCert)
-	}, app).listen(global.config.httpsPort)
-}
-if (global.config.httpPort !== 0) {
-	var http = require('http').createServer(app).listen(global.config.httpPort);
-	global.io = require('socket.io')(http);
-}
-var streamOutput = require("../streams/httphandler.js"),
-	streamInput = require("../streams/inputhandler.js")
+let app = require("express")()
+let fs = require("fs")
 
-app.enable('trust proxy');
-app.use(function(req, res, next) {
-	res.setHeader('X-Powered-By', 'Cast 1.0')
-	res.setHeader('Server', "Cast/1.0")
-	next()
+global.io = require("socket.io")
+
+if (config.httpsPort !== 0) {
+    let https = require("http2").createServer({
+        key: fs.readFileSync(config.httpsKey),
+        cert: fs.readFileSync(config.httpsCert),
+    }, app).listen(global.config.httpsPort)
+    global.io.attach(https)
+}
+if (config.httpPort !== 0) {
+    let http = require("http").createServer(app).listen(config.httpPort);
+    global.io.attach(http)
+}
+
+let streamOutput = require("../streams/httphandler.js")
+let streamInput = require("../streams/inputhandler.js")
+
+if (config.trustProxy) {
+    app.enable("trust proxy");
+}
+
+app.use((req, res, next) => {
+    res.setHeader("X-Powered-By", "Cast 1.0")
+    res.setHeader("Server", "Cast/1.0")
+    next()
 })
 
-var files = fs.readdirSync(global.localdir + "/intern/HTTP/apps/")
-for (var id in files) {
-	if (files.hasOwnProperty(id)) {
-		require(global.localdir + "/intern/HTTP/apps/" + files[id])(app)
-	}
+for (let file of fs.readdirSync(global.localdir + "/intern/HTTP/apps/")) {
+    require(global.localdir + "/intern/HTTP/apps/" + file)(app)
 }
-
-var files = fs.readdirSync(global.localdir + "/hooks/http/") //load HTTP hooks
-for (var id in files) {
-	if (files.hasOwnProperty(id)) {
-		require(global.localdir + "/hooks/http/" + files[id])(app)
-	}
+for (let file of fs.readdirSync(global.localdir + "/hooks/http/")) {
+    require(global.localdir + "/hooks/http/" + file)(app)
 }
-
 
 streamOutput(app)
 streamInput(global.config)
