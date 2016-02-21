6800bef7b2ae158155d9cde56391017a83fd5b43
Maartje Eyskens <meyskens@me.com>
2016-02-21 20:19:09 +0100

    remove global.

--- intern/geolock/geolock.js
+++ intern/geolock/geolock.js
@@ -1,20 +1,20 @@
-if (global.config.geolock && global.config.geolock.enabled && !global.maxmind) {
+if (config.geolock && config.geolock.enabled && !maxmind) {
     global.maxmind = require("maxmind")
-    if (!global.maxmind.init(config.geolock.maxmindDatabase)) {
+    if (!maxmind.init(config.geolock.maxmindDatabase)) {
         console.log("Error loading Maxmind Database")
     }
 }
 
 export function isAllowed(ip) {
-    if (typeof global.config.geolock === "undefined" || !global.config.geolock.enabled) {
+    if (typeof config.geolock === "undefined" || !config.geolock.enabled) {
         return true
     }
-    let ipLocation = global.maxmind.getLocation(ip)
-    let isWhilelistMode = global.config.geolock.mode === "whitelist"
+    let ipLocation = maxmind.getLocation(ip)
+    let isWhilelistMode = config.geolock.mode === "whitelist"
     if (!ipLocation) {
         return isWhilelistMode
     }
-    if (global.config.geolock.countryCodes.indexOf(ipLocation.countryCode) === -1) {
+    if (config.geolock.countryCodes.indexOf(ipLocation.countryCode) === -1) {
         return !isWhilelistMode
     }
     return isWhilelistMode
