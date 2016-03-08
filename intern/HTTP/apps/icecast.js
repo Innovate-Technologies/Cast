202bd18a424b6c92f21919d5d5a47bf8e322f9b3
Maartje Eyskens <meyskens@me.com>
2016-03-08 15:26:53 +0100

    Refactor the Icecast 2.4 API

--- intern/HTTP/apps/icecast.js
+++ intern/HTTP/apps/icecast.js
@@ -1,29 +1,27 @@
-module.exports = function(app) {
+module.exports = (app) => {
 
-    app.get("/status-json.xsl", function(req, res) {
+    app.get("/status-json.xsl", (req, res) => {
         var streams = global.streams.getActiveStreams()
         var iceStreams = []
-        for (var id in streams) {
-            if (streams.hasOwnProperty(id)) {
-                iceStreams.push({
-                    "audio_info": "bitrate=" + (global.streams.getStreamConf(streams[id]).bitrate || 0) + ";",
-                    "bitrate": global.streams.getStreamConf(streams[id]).bitrate || 0,
-                    "channels": 2, //we guess so
-                    "genre": global.streams.getStreamConf(streams[id]).genre,
-                    "listener_peak": global.streams.numberOfListerners(streams[id]),
-                    "listeners": global.streams.numberOfListerners(streams[id]),
-                    "listenurl": global.config.hostname + "/streams/" + streams[id],
-                    "samplerate": 44100, //we guess
-                    "server_description": "",
-                    "server_name": global.streams.getStreamConf(streams[id]).name || "",
-                    "server_type": global.streams.getStreamConf(streams[id]).type || "",
-                    "server_url": global.streams.getStreamConf(streams[id]).url || "",
-                    "stream_start": "Fri, 03 Jul 2015 13:13:18 -0400", //leaving that for now
-                    "stream_start_iso8601": "2015-07-03T13:13:18-0400", //leaving that for now
-                    "title": global.streams.getStreamMetadata(streams[id]).song || "",
-                    "dummy": null
-                })
-            }
+        for (let stream of streams) {
+            iceStreams.push({
+                "audio_info": "bitrate=" + (global.streams.getStreamConf(stream).bitrate) + ";",
+                "bitrate": global.streams.getStreamConf(streams).bitrate,
+                "channels": 2, // we guess so, there is currently not format reading
+                "genre": global.streams.getStreamConf(stream).genre,
+                "listener_peak": global.streams.numberOfListerners(stream),
+                "listeners": global.streams.numberOfListerners(stream),
+                "listenurl": global.config.hostname + "/streams/" + stream,
+                "samplerate": 44100, // we guess so, there is currently not format reading
+                "server_description": "",
+                "server_name": global.streams.getStreamConf(stream).name,
+                "server_type": global.streams.getStreamConf(stream).type,
+                "server_url": global.streams.getStreamConf(stream).url,
+                "stream_start": "Fri, 03 Jul 2015 13:13:18 -0400", // leaving that for now
+                "stream_start_iso8601": "2015-07-03T13:13:18-0400", // leaving that for now
+                "title": global.streams.getStreamMetadata(stream).song,
+                "dummy": null,
+            })
         }
 
         if (iceStreams.length === 1) {
@@ -32,14 +30,14 @@ module.exports = function(app) {
 
         res.json({
             "icestats": {
-                "admin": "cast@shoutca.st",
+                "admin": "nobody@getca.st",
                 "host": global.config.hostname.replace("http://", "").replace("https://", ""),
-                "location": "Cloud", //Why?
+                "location": "Cloud",
                 "server_id": "Cast 1.0",
-                "server_start": "Fri, 03 Jul 2015 09:09:19 -0400", //leaving that for now
-                "server_start_iso8601": "2015-07-03T09:09:19-0400", //leaving that for now
-                "source": iceStreams
-            }
+                "server_start": "Fri, 03 Jul 2015 09:09:19 -0400", // leaving that for now
+                "server_start_iso8601": "2015-07-03T09:09:19-0400", // leaving that for now
+                "source": iceStreams,
+            },
         })
 
     })
