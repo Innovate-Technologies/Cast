68fa772b5809607b803307900aabdda7497afe68
Maartje Eyskens <meyskens@me.com>
2016-06-20 22:29:22 +0200

    Added debug logs to get info in production

--- hooks/action/icecast-yp.js
+++ hooks/action/icecast-yp.js
@@ -14,6 +14,7 @@ let sendRequest = (host, data, callback) => {
             return callback(new Error("Got an invalid response"));
         }
         callback(null, response.headers);
+	console.log(response.headers);
     }).on("timeout", () => {
         callback(new Error("Request timed out"));
     });
