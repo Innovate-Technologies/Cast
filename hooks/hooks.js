faab31c68197b4112f870e3f4c9d50adc3cb0552
Maartje Eyskens <meyskens@me.com>
2016-08-20 15:46:52 +0200

    Fix feedback

--- hooks/hooks.js
+++ hooks/hooks.js
@@ -6,7 +6,7 @@ global.events = new Events();
 
 const actionModules = fs.readdirSync(global.localdir + "/hooks/action");
 for (let module of actionModules) {
-    if (fs.statSync(`${global.localdir}/hooks/action/${module}`).isFile()){
+    if (fs.statSync(`${global.localdir}/hooks/action/${module}`).isFile()) {
         require(`${global.localdir}/hooks/action/${module}`);
     }
 }
